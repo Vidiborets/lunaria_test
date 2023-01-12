a2dab202eb10cef1a40feb4d7e22e7f3c880b189
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2023-01-12 13:00:29 +0000

    Deploying to gh-pages from @ Vidiborets/lunaria_test@c038ef7c8602c311d5f91be6b37f1ebe8f58eb66 🚀

---
+++ index.1987f71b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".main-carousel");new Flickity(e,{contain:!0,wrapAround:!0,prevNextButtons:!1,pageDots:!1,freeScroll:!0,freeScrollFriction:.03,autoPlay:1e3})}();
+//# sourceMappingURL=index.1987f71b.js.map
